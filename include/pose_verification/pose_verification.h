7534c205cf0309d3f4a0ecb458737231935bc3b1
Manuel Vogel <ge87xig@mytum.de>
2024-03-04 22:58:11 +0000

    receiving point cloud and humans now, conversion to pcl done

--- include/pose_verification/pose_verification.h
+++ include/pose_verification/pose_verification.h
@@ -4,13 +4,15 @@
 #include <vector>
 
 #include <ros/ros.h>
+#include <tf2_ros/transform_broadcaster.h>
 #include <tf2_ros/buffer.h>
 #include <tf2_ros/transform_listener.h>
-#include "tf2_geometry_msgs/tf2_geometry_msgs.h"
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #include <tf/transform_broadcaster.h>
 #include <geometry_msgs/Point.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <concert_msgs/Humans.h>
+#include <gazebo_msgs/ModelStates.h>
 
 
 class PoseVerification
@@ -18,21 +20,26 @@ class PoseVerification
 
 public:
 
-    PoseVerification();
+    PoseVerification(ros::NodeHandle nh);
 
     void velodyneCallback(const sensor_msgs::PointCloud2ConstPtr& msg);
 
     void humanJointCallback(const concert_msgs::HumansConstPtr& msg);
 
+    void modelStatesCallback(const gazebo_msgs::ModelStates::ConstPtr& msg);
+
 
 private:
 
-    ros::NodeHandle nh;
+    ros::NodeHandle _nh;
 
     /**
      * @brief Human measurement points to be used in sara shield
      */
-    std::vector<geometry_msgs::Point> _human_meas;
+    std::vector<geometry_msgs::Point> _human_meas[2];
+    //TODO: fix for any amount of humans
+
+    double _human_meas_time[2];
 
 
     /**
@@ -56,6 +63,13 @@ private:
      */
     ros::Subscriber _velodyne_sub;
 
+
+    /**
+     * @brief Ros subscriber to receive Robot position and orientation
+     */
+    ros::Subscriber _model_state_sub;
+
+    ros::Publisher _velodyne_human_pub;
 
 };
 
--- src/pose_verification.cc
+++ src/pose_verification.cc
@@ -1,35 +1,61 @@
 #include "pose_verification/pose_verification.h"
 
+#include <pcl_ros/transforms.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/PCLPointCloud2.h>
 
-PoseVerification::PoseVerification():
-  nh{},
-  _human_joint_sub(nh.subscribe("/sara_shield/human_pose_measurement", 100, &PoseVerification::humanJointCallback, this)),
-  _velodyne_sub(nh.subscribe("/VLP16_lidar_front/velodyne_points", 100, &PoseVerification::velodyneCallback, this)),
+
+PoseVerification::PoseVerification(ros::NodeHandle nh):
+  _nh(nh),
+  _human_joint_sub(_nh.subscribe("/sara_shield/human_pose_measurement", 100, &PoseVerification::humanJointCallback, this)),
+  _velodyne_sub(_nh.subscribe("/VLP16_lidar_front/velodyne_points", 100, &PoseVerification::velodyneCallback, this)),
+  _model_state_sub(_nh.subscribe("/gazebo/model_states", 100, &PoseVerification::modelStatesCallback, this)),
+  _velodyne_human_pub(_nh.advertise<sensor_msgs::PointCloud2>("/TEST/velodyne", 100)),
   _tfListener(_tfBuffer)
 {
 }
 
 
-
 void PoseVerification::velodyneCallback(const sensor_msgs::PointCloud2ConstPtr& msg) {
   // get the lidar point cloud
   std::cout<<"Point Cloud Received"<<std::endl;
-  std::cout<<"Size of Point cloud:"<<msg->width * msg->height<<std::endl;
+  std::cout<<"Size of Point cloud:"<<msg->width <<"x"<< msg->height<<std::endl;
+  
+  // debug output point cloud 
+  sensor_msgs::PointCloud2 humanPointCloud;
+  int count = 0;
+
+  // convert to pcl for more efficient point cloud handling
+  pcl::PCLPointCloud2 pcl_pc2;
+  pcl_conversions::toPCL(*msg, pcl_pc2);
+
+  for(int human_index = 0; human_index < 2; human_index++){
+    for(geometry_msgs::Point& humanPoint:_human_meas[human_index]){
+      // TODO find all points close to a human 
+    }
+  }
+   
+
+  // send out debug pointcloud
+  humanPointCloud.header = msg->header;
+  humanPointCloud.width = count;
+  humanPointCloud.height = 1;
+  _velodyne_human_pub.publish(humanPointCloud);
 }
 
 
 void PoseVerification::humanJointCallback(const concert_msgs::HumansConstPtr& msg) {
-  // get the robot position
-  std::cout<<"Human received"<<std::endl;
+  // get the world to LiDAR transformation 
   geometry_msgs::TransformStamped transformation;
-  std::string source_frame =  msg->header.frame_id;
+  std::string target_frame = "VLP16_lidar_front";
+  std::string source_frame = msg->header.frame_id;
   if(source_frame == ""){
     source_frame="base_link";
   }
 
   try {
     transformation = _tfBuffer.lookupTransform(
-        "base_link", source_frame, msg->header.stamp, ros::Duration(0.003));
+        target_frame, source_frame, msg->header.stamp, ros::Duration(0.02));
   } catch (tf2::LookupException const&) {
     ROS_WARN("NO TRANSFORM FOUND (Lookup failed)");
     return;
@@ -38,39 +64,69 @@ void PoseVerification::humanJointCallback(const concert_msgs::HumansConstPtr& ms
     return;
   }
 
-  //get all human measurment points and transform them to robot coordinate system
-  if(msg->humans.size() > 0)
-  {
+  //get all human measurment points and transform them to the LiDAR coordinate system
+  if(msg->humans.size() > 0){
     for (const concert_msgs::Human3D &human: msg->humans){
-      _human_meas.clear();
       int human_index = human.label_id;
-      for(const concert_msgs::Keypoint3D &keypoint:human.keypoints)
-      {
+      _human_meas[human_index].clear();
+      for(const concert_msgs::Keypoint3D &keypoint:human.keypoints){
         geometry_msgs::PointStamped pointStamped;
         geometry_msgs::PointStamped pointStampedLocal;
-        pointStamped.point = keypoint.pose.position;    
+        pointStamped.point = keypoint.pose.position;
+        // transform human points to lidar coordinates
         tf2::doTransform(pointStamped, pointStampedLocal, transformation);
         geometry_msgs::Point pointLocal = pointStampedLocal.point;
         
-        _human_meas.emplace_back(pointLocal);
+        _human_meas[human_index].emplace_back(pointLocal);
+        _human_meas_time[human_index] = msg->header.stamp.toSec();
 
       }
-      double messageTime = msg->header.stamp.toSec();
-      if(messageTime == 0.0){
-        messageTime = ros::Time::now().toSec();
-      }
-      // _shield.humanMeasurement(_human_meas, human_index, messageTime);
-
+      std::cout<<"Human received"<<std::endl;
     }
-  }
-  
+  } 
 }
 
 
-int main(int argc, char * argv[])
-{
-    ros::init(argc, argv, "pose_verification_node");
-    PoseVerification test();
-    ros::spin();
-    return 0;
+
+//convert the gazebo transformation between world and base_link into a tf
+void PoseVerification::modelStatesCallback(const gazebo_msgs::ModelStates::ConstPtr& msg) {
+  ROS_DEBUG("Incoming Transform");
+  // find the index of the robot transform in the list of transforms
+  int index = -1;
+  for (uint64_t i = 0; i < msg->name.size(); i++) {
+    if (msg->name[i] == "ModularBot") {
+      index = i;
+      break;
+    }
+  }
+
+  if (index == -1) {
+    ROS_ERROR("Could not find robot modular_robot in model states message");
+    return;
+  }
+  static tf2_ros::TransformBroadcaster br;
+
+  // make a tf transform out of the given gazebo transform
+  geometry_msgs::TransformStamped TransformStamped;
+  TransformStamped.header.stamp = ros::Time::now();
+  TransformStamped.header.frame_id = "world";
+  TransformStamped.child_frame_id = "base_link";
+  TransformStamped.transform.translation.x = msg->pose[index].position.x;
+  TransformStamped.transform.translation.y = msg->pose[index].position.y;
+  TransformStamped.transform.translation.z = msg->pose[index].position.z;
+  TransformStamped.transform.rotation = msg->pose[index].orientation;
+
+  //publish the transform
+  br.sendTransform(TransformStamped);
+}
+
+
+
+int main(int argc, char * argv[]){
+  ros::init(argc, argv, "pose_verification_node");
+  ros::NodeHandle nh;
+  PoseVerification pose(nh);
+  ROS_WARN("Start");
+  ros::spin();
+  return 0;
 }
